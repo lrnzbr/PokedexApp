a66e0ceadea57071a1494232f63930c9691c7c77
Lorenzo Brown <lrnzbr@gmail.com>
2023-02-07 19:21:48 -0600

    Initial Commit

---
+++ PokedexApp/PokedexAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexAppApp.swift
+//  PokedexApp
+//
+//  Created by Lorenzo Brown on 2/7/23.
+//
+
+import SwiftUI
+
+@main
+struct PokedexAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
